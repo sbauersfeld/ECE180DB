cfa79cbd9e0172347eaef188ad9225e25e631d3f
Scott <sbauersfeld@g.ucla.edu>
2020-01-30 14:15:03 -0800

    led fix

--- leds.py
+++ leds.py
@@ -3,7 +3,6 @@ import neopixel
 import time
 
 pixels = neopixel.NeoPixel(board.D18, 12)
-while True:
-  #pixels.fill((255, 0, 0))
-  pixels[0] = (255, 0, 0)
-  #time.sleep(1)
+#pixels.fill((255, 0, 0))
+pixels[0] = (255, 0, 0)
+pixels.show()
