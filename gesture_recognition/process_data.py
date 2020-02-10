91a2f97e10f3d7a85b379aa14fee5222e095e6c4
WilsonLin9608 <wlin9608@gmail.com>
2020-02-10 11:41:35 +0000

    changed feature selections

--- gesture_recognition/process_data.py
+++ gesture_recognition/process_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.preprocessing import Normalizer
 
 def get_header():
     header = []
@@ -10,29 +11,38 @@ def get_header():
 
 def get_features(series):
     features = []
+    # may change
+    transformer = Normalizer().fit([series])
+    series = transformer.transform([series])[0]
+
     features.append(max(series))
     features.append(min(series))
     features.append(max(series) - min(series))
     features.append(series.mean())
     features.append(series.std())
+
     return features
 
 
 def get_model_features(trace):
-    features = []
-    # normalize
-    trace["Accel"] = np.linalg.norm(
-        (trace["Accel_x"], trace["Accel_y"], trace["Accel_z"]),
-        axis=0)
-    trace["Gyro"] = np.linalg.norm(
-        (trace["Gyro_x"], trace["Gyro_y"], trace["Gyro_z"]),
-        axis=0)
-    trace["Mag"] = np.linalg.norm(
-        (trace["Mag_x"], trace["Mag_y"], trace["Mag_z"]),
-        axis=0)
+    # features = []
+    # # normalize
+    # trace["Accel"] = np.linalg.norm(
+    #     (trace["Accel_x"], trace["Accel_y"], trace["Accel_z"]),
+    #     axis=0)
+    # trace["Gyro"] = np.linalg.norm(
+    #     (trace["Gyro_x"], trace["Gyro_y"], trace["Gyro_z"]),
+    #     axis=0)
+    # trace["Mag"] = np.linalg.norm(
+    #     (trace["Mag_x"], trace["Mag_y"], trace["Mag_z"]),
+    #     axis=0)
 
+    # for sensor in ["Accel", "Gyro", "Mag"]:
+    #     features.extend(get_features(trace[sensor]))
+    features = []
     for sensor in ["Accel", "Gyro", "Mag"]:
-        features.extend(get_features(trace[sensor]))
+        for axis in ["_x", "_y", "_z"]:
+            features.extend(get_features(trace[sensor + axis]))
 
     return features
 
