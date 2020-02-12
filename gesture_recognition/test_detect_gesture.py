48a1f51973583942005f1579e1f4bb6f4c3125f5
WilsonLin9608 <wlin9608@gmail.com>
2020-02-12 00:19:41 +0000

    test detect gestures

--- gesture_recognition/test_detect_gesture.py
+++ gesture_recognition/test_detect_gesture.py
@@ -9,42 +9,69 @@ import process_data as pdata
 import numpy as np
 
 
-model = joblib.load(prefix + 'models/' + member + '/' + model_name + ".joblib")
-scaler = joblib.load(prefix + 'models/' + member + '/' + scaler_name + ".joblib")
+model = joblib.load('models/wilson/model.joblib')
+scaler = joblib.load('models/wilson/scaler.joblib')
 
 IMU.detectIMU()
 IMU.initIMU()
     
 
-CHECK_TIME_INCREMENT_MS = 200
-SAMPLE_SIZE_MS = 840
-
 header = ["time_ms"] + pdata.get_header()
-data = collections.deque(maxlen=int(SAMPLE_SIZE_MS / 10)) #10 Hz
-
-start = datetime.datetime.now()
-elapsed_ms = 0
-last_classified = 0
-last_classification = "negative"
+duration_s = 1.5
 
 while True:
-    row = [elapsed_ms] + read_sensor()
-    data.append(row)
+    input("Press 'Enter' to start tracing...")
+    start = datetime.datetime.now()
+    elapsed_ms = 0
+    data = []
 
-    if elapsed_ms - last_classified >= CHECK_TIME_INCREMENT_MS and len(data) == data.maxlen:
-        df = pd.DataFrame(list(data), columns=header)
-        features = pdata.get_model_features(df)
-        features = scaler.transform(np.reshape(features, (1, -1)))
-        prediction = model.predict(features)[0]
+    while elapsed_ms < duration_s * 1000:
+        print("tracing...")
 
-        print(int(elapsed_ms), prediction)
-        if prediction != 'negative':# and last_classification != prediction:
-            print("========================>", prediction)
-            input("press 'Enter' to continue...")
-            # return prediction
-        data.clear()
+        row = [elapsed_ms] + read_sensor()
+        data.append(row)
+        elapsed_ms = (datetime.datetime.now() - start).total_seconds() * 1000
 
-        last_classified = elapsed_ms
-        last_classification = prediction
+    df = pd.DataFrame(data, columns=header)
+    features = pdata.get_model_features(df)
+    features = scaler.transform(np.reshape(features, (1, -1)))
+    prediction = model.predict(features)[0]
+    print(prediction)
 
-    elapsed_ms = (datetime.datetime.now() - start).total_seconds() * 1000
+
+
+# CHECK_TIME_INCREMENT_MS = 400
+# SAMPLE_SIZE_MS = 850
+
+# header = ["time_ms"] + pdata.get_header()
+# data = collections.deque(maxlen=int(SAMPLE_SIZE_MS / 10)) #10 Hz
+
+# start = datetime.datetime.now()
+# elapsed_ms = 0
+# last_classified = 0
+# last_classification = "negative"
+
+# while True:
+#     row = [elapsed_ms] + read_sensor()
+#     data.append(row)
+
+#     if elapsed_ms - last_classified >= CHECK_TIME_INCREMENT_MS and len(data) == data.maxlen:
+#         df = pd.DataFrame(list(data), columns=header)
+#         features = pdata.get_model_features(df)
+#         features = scaler.transform(np.reshape(features, (1, -1)))
+#         prediction = model.predict(features)[0]
+
+#         print(int(elapsed_ms), prediction)
+#         if prediction != 'negative':# and last_classification != prediction:
+#             print("========================>", prediction)
+#             input("press 'Enter' to continue...")
+#             # return prediction
+#         data.clear()
+
+#         last_classified = elapsed_ms
+#         last_classification = prediction
+
+#     elapsed_ms = (datetime.datetime.now() - start).total_seconds() * 1000
+
+
+   
