188cff7f88c65707a35a7ab18b9596933bff4d1d
Scott <sbauersfeld@g.ucla.edu>
2020-02-13 15:07:40 -0800

    changed leds to half bightness

--- range_detection/leds.py
+++ range_detection/leds.py
@@ -3,10 +3,5 @@ import neopixel
 
 num_pixels = 12
 pixels = neopixel.NeoPixel(board.D18, num_pixels)
-pixels.fill((255, 0, 0))
-# pixels[0] = (0, 255, 0)
-
-#for i in range(num_pixels):
- # pixels[i] = (0, 255, 0)
-
+pixels.fill((127, 0, 0))
 pixels.show()
