57efc1f21bb4bcc8b0eb64311f95fbe99564432f
Scott <sbauersfeld@g.ucla.edu>
2020-02-18 00:40:56 -0800

    detect audio from bluetooth mic

--- speech_detection/speech_detection.py
+++ speech_detection/speech_detection.py
@@ -1,18 +1,33 @@
 import speech_recognition as sr
 
-# Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+m = None
+print("Showing available microphones...")
+for i, microphone_name in enumerate(sr.Microphone.list_microphone_names()):
+    print(microphone_name)
+    if microphone_name == "Headset (SoundBuds Slim Hands-F":
+        print("Setting up bluetooth microphone!")
+        m = sr.Microphone(device_index=i)
+        break
 
-# Speech recognition using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+if m is None:
+    print("Using default microphone.")
+    m = sr.Microphone()
+
+print()
+r = sr.Recognizer()
+
+while True:
+    with m as source:
+        print("Say something! 'end' will end this session.")
+        audio = r.listen(source)
+
+        try:
+            command = r.recognize_google(audio)
+            print("You said: " + command)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        if command == "end" or command == "and":
+            break
